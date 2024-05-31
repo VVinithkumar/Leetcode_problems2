ec74b24c467b8fe3bf5593126e92a91428fd5d7d
VVinithkumar <142335228+VVinithkumar@users.noreply.github.com>
2024-05-31 09:43:09 +0530

    Time: 1 ms (99.93%) | Memory: 42 MB (50.43%) - LeetSync

--- 3312-number-of-changing-keys/number-of-changing-keys.java
+++ 3312-number-of-changing-keys/number-of-changing-keys.java
@@ -3,11 +3,7 @@ class Solution {
         int sum =0;
         for(int i=0;i<s.length()-1;i++)
         {
-            if(Character.toLowerCase(s.charAt(i))==Character.toLowerCase(s.charAt(i+1)))
-            {
-                continue;
-            }
-            else
+            if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(i+1)))
             {
                 sum++;
             }
