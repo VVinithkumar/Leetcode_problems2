abec94201e0d510d661182876cfa5c102c771830
VVinithkumar <142335228+VVinithkumar@users.noreply.github.com>
2024-06-19 10:23:45 +0530

    Time: 199 ms (70.30%) | Memory: 41.7 MB (39.25%) - LeetSync

--- 3422-find-the-n-th-value-after-k-seconds/find-the-n-th-value-after-k-seconds.java
+++ 3422-find-the-n-th-value-after-k-seconds/find-the-n-th-value-after-k-seconds.java
@@ -1,11 +1,8 @@
 class Solution {
     public int valueAfterKSeconds(int n, int k) {
       int arr[]=new int[n];
+      Arrays.fill(arr,1);
       int v=0;
-      for(int i=0;i<n;i++)
-      {
-         arr[i]=1;
-      }
       while(v<k)
       {
          for(int i=0;i<arr.length-1;i++)
